e8f5eff0ef6fdb1d2f4b97d04fac445034a1ca06
Rahul Joshi <rahuljoshi@Rahuls-MBP.home>
2016-10-02 01:07:05 -0400

    Have the basic player movement with a capsule and a cube ground.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	CharacterController cControl;
+
+	const float lookSpeed = 500;
+	const float moveSpeed = 500;
+
+	// Use this for initialization
+	void Start () {
+		cControl = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Getting inputs
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveZ = Input.GetAxis ("Vertical");
+		//To look left an right, rotate about the Y axis
+		float lookX = -Input.GetAxis ("Mouse Y") * Time.deltaTime * lookSpeed;
+		//To look up and down, rotate about the X axis
+		float lookY = -Input.GetAxis ("Mouse X") * Time.deltaTime * lookSpeed;
+
+		//Move the character
+		cControl.SimpleMove((transform.forward * moveZ + transform.right * moveX) * Time.deltaTime * moveSpeed);
+
+		//Turn the chatacter
+		transform.Rotate(lookX, lookY, 0f);
+		//Make sure transform.Rotate does not create any z axis rotation when the player
+		//moves the mouse in a circle
+		Vector3 zLock = new Vector3 (this.transform.eulerAngles.x, this.transform.eulerAngles.y, 0f);
+		this.transform.eulerAngles = zLock;
+	}
+}
